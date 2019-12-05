c7c3b6ef0f2875c4d934d284a4350989bb7bad93
George Eastlick <geast2@hotmail.com>
2019-12-04 19:49:34 -0600

    styling, info, about

---
+++ js/site.js
@@ -0,0 +1,28 @@
+function loadContent(page) {
+    $.ajax({
+        url: 'header.html',
+        context: document.body,
+        success: function (data) {
+            $('#header').html(data);
+        }
+    });
+    $.ajax({
+        url: 'navbar.html',
+        context: document.body,
+        success: function (data) {
+            $('#navbar').html(data);
+
+            if (page === "home") $("#home_li").addClass("active");
+            else if (page === "info") $("#info_li").addClass("active");
+            else if (page === "about") $("#about_li").addClass("active");
+        }
+    });
+    $.ajax({
+        url: 'footer.html',
+        context: document.body,
+        success: function (data) {
+            $('#footer').html(data);
+        }
+    });
+
+}
