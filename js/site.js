3dd5fc8fe649b4a90a64b8e5657c45de30d10989
George Eastlick <geast2@hotmail.com>
2019-12-09 08:50:08 -0600

    added standard sized carousel images.  modified loadContent so that page displays all at once.

--- js/site.js
+++ js/site.js
@@ -1,28 +1,27 @@
 function loadContent(page) {
-    $.ajax({
-        url: 'header.html',
-        context: document.body,
-        success: function (data) {
-            $('#header').html(data);
-        }
-    });
-    $.ajax({
-        url: 'navbar.html',
-        context: document.body,
-        success: function (data) {
-            $('#navbar').html(data);
+    /*
+    const loading=$(`
+        <div class="d-flex align-items-center">
+        <strong>Loading...</strong>
+        <div class="spinner-border ml-5 text-primary" role="status" aria-hidden="true"></div>
+        </div>
+    `).prependTo('body');
+    */
+    $.when(
+        $.ajax('header.html'),
+        $.ajax('navbar.html'),
+        $.ajax('footer.html')
+    ).done(function (hdr, nav, ftr) {
+        /*loading.remove();*/
+        $('#header').html(hdr[0]);
+        $('#navbar').html(nav[0]);
+        if (page === "home") $("#home_li").addClass("active");
+        else if (page === "contact") $("#contact_li").addClass("active");
+        else if (page === "about") $("#about_li").addClass("active");
+        $('#content').removeClass('invisible');
+        $('#footer').html(ftr[0]);
 
-            if (page === "home") $("#home_li").addClass("active");
-            else if (page === "contact") $("#contact_li").addClass("active");
-            else if (page === "about") $("#about_li").addClass("active");
-        }
-    });
-    $.ajax({
-        url: 'footer.html',
-        context: document.body,
-        success: function (data) {
-            $('#footer').html(data);
-        }
+
     });
 
 }
